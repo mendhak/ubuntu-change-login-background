5635ae40ce8a4b7a0359eaf3d58229c7d2717682
mendhak <mendhak@users.noreply.github.com>
2024-09-15 21:01:50 +0100

    Fix paths

--- set_both_wallpapers.sh
+++ set_both_wallpapers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
-echo "Processing $1" | tee /home/mendhak/change-login-background/run.log
+echo "Processing $1" | tee /home/mendhak/Projects/change-login-background/run.log
 
-echo "Creating blurred version" | tee -a /home/mendhak/change-login-background/run.log
+echo "Creating blurred version" | tee -a /home/mendhak/Projects/change-login-background/run.log
 blurredpath=/tmp/blurred.jpg
 convert $1 -channel RGBA -blur 0x26 $blurredpath
 
 # Set the lock screen wallpaper
-sudo /home/mendhak/change-login-background/change.sh "$blurredpath" 2>&1 | tee -a /home/mendhak/change-login-background/run.log
+sudo /home/mendhak/Projects/change-login-background/change.sh "$blurredpath" 2>&1 | tee -a /home/mendhak/Projects/change-login-background/run.log
 
 # Now let Variety set the desktop wallpaper as usual.
 ~/.config/variety/scripts/set_wallpaper $@
