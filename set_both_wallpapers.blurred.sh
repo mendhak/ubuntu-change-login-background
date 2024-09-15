c404e7679559d69f1009bae87036f8def403dee9
mendhak <mendhak@users.noreply.github.com>
2024-09-15 21:56:46 +0100

    Blurred and non blurred versions of the scripts

---
+++ set_both_wallpapers.blurred.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Processing $1" | tee $(dirname $0)/run.log
+
+echo "Creating blurred version" | tee -a $(dirname $0)/run.log
+blurredpath=/tmp/blurred.jpg
+convert $1 -channel RGBA -blur 0x26 $blurredpath
+
+# Set the lock screen wallpaper
+sudo $(dirname $0)/change.sh "$blurredpath" 2>&1 | tee -a $(dirname $0)/run.log
+
+# Now let Variety set the desktop wallpaper as usual.
+~/.config/variety/scripts/set_wallpaper $@
--- sync_desktop_wallpaper_to_login.blurred.sh
+++ sync_desktop_wallpaper_to_login.blurred.sh
@@ -1,3 +1,5 @@
 current_wallpaper_uri=$(gsettings get org.gnome.desktop.background picture-uri | sed "s/'//g")
 current_wallpaper_path=$(python3 -c "import sys;from urllib.parse import unquote, urlparse; print(unquote(urlparse(sys.argv[1]).path))" "$current_wallpaper_uri")
-sudo ./change.sh "$current_wallpaper_path"
+blurredpath=/tmp/blurred.jpg
+convert $current_wallpaper_path -channel RGBA -blur 0x26 $blurredpath
+sudo ./change.sh "$blurredpath"
